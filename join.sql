6b332cc6d398f4bb899c1cded63c7de12ac4298c
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-05-15 14:58:18 +0200

    5 j

--- join.sql
+++ join.sql
@@ -38,7 +38,14 @@ ORDER BY `students`.`surname` ASC , `students`.`name` ASC;
 
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
-
+SELECT `degrees`.`name` as `degree_name` , `courses`.`name` as `course_name` , `teachers`.`name` as `teacher_name` , `teachers`.`surname` as `teacher_surname`
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
 
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
